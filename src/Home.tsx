75b58f6107f270d2eb5f99b6f04aaa739984a337
Dab-9270 <moidossantos01@gmail.com>
2025-02-20 12:08:45 +0100

    composant page home
    Co-authored-by: Khephren <Kamusubi@users.noreply.github.com>
    Co-authored-by: Ethan <EthanLegoat@users.noreply.github.com>

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,8 +1,15 @@
 import { LogoTitle } from './component/Component'
 import {CreditButton} from './component/Component'
 import green from './assets/monster/green.svg'
+import bibabo from './assets/monster/bibabo.svg'
 
 import {SelectMode} from './component/SelectMode'
+import {SetProfile} from './component/SetProfile'
+import {RuleBlox} from './component/Component'
+import {PlayButton} from './component/Component'
+import {ArtifactsList} from './component/Component'
+import {Footer} from './component/Component'
+
 import './style/wikispeed.css'
 
 function Home() {
@@ -10,10 +17,21 @@ function Home() {
   return (
 
         <body>
-              {/* <LogoTitle />
                 <CreditButton />
-                 */}
-                 <SelectMode title='Solo' img={green}/>
+               <LogoTitle />
+
+                 <div className="selection">                 
+                    <SelectMode title='Solo' img={green}/>
+                    <SetProfile image={green} username={undefined} />
+                    <SelectMode title='Multijoueur' img={bibabo}/> 
+
+                 </div>
+
+                  <RuleBlox content='1. Crées une partie Lance une partie en solo ou avec des amis.' /> 
+                  <Footer content1='Aide' content2='Confidentialités' content3='Mentions Légales'></Footer>
+                 <PlayButton/>
+
+                 <ArtifactsList/>
         </body>
       
       
--- src/component/Component.tsx
+++ src/component/Component.tsx
@@ -1,4 +1,6 @@
 import lightning from '../assets/Lightning.svg'
+import mine from '../assets/artifact/mine.svg'
+import map from '../assets/artifact/map.svg'
 
 export const LogoTitle = () =>{
     return <div className='logo-container'>
@@ -15,4 +17,46 @@ export const CreditButton = () =>{
     return <div className='credit-button'>
       <p className='manjari'>Credits</p>
       </div>
+}
+const Artifacts = (props:{img:string}) =>{
+    return <div className='artifact-border'>
+        <figure> <img src={props.img} alt="" /></figure>
+      </div>
+}
+
+export const ArtifactsList = () => {
+  const artifactImages = [mine,map
+    
+  ]; 
+
+  return (
+    <div className="artifacts-list">
+      {artifactImages.map((img, index) => (
+        <Artifacts key={index} img={img} />
+      ))}
+    </div>
+  );
+};
+
+
+
+export const RuleBlox = (props :{content : string}) =>{
+    return <div className='rule-bloc'>
+      <p className='rules'> {props.content}</p>
+      </div>
+} 
+
+export const PlayButton = () =>{
+  return<button className='PlayButton'> Play ! →</button>
+} 
+
+export const Footer = (props :{content1 : string, content2 : string, content3 : string}) => {
+  return (
+    <div className='footer'>
+      <p> {props.content1}</p>
+      <p> {props.content2}</p>
+      <p> {props.content3}</p>
+    </div>
+  );
+
 }
