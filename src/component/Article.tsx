7cf2dfeb78587162ac8a97b7f656ae7da8a7e3e5
Kamusubi <khephdjelidi@gmail.com>
2025-02-23 23:33:34 +0100

    articile wikipedia (manque le style de l'infobox)

--- src/component/Article.tsx
+++ src/component/Article.tsx
@@ -1,6 +1,24 @@
-export const ArticleDisplayer = (props:{title:string, content:string}) =>{
-    return <div className='article-container'>
-        <h2 className='manjari'>{props.title}</h2>
-        <p className='manjari'>{props.content}</p>
-      </div>
-}
+import { useEffect, useState } from 'react';
+
+export const ArticleDisplayer = (props: {title: string}) => {
+    const [content, setContent] = useState<string>('');
+
+    useEffect(() => {
+        const fetchArticle = async () => {
+              const response = await fetch(`https://fr.wikipedia.org/api/rest_v1/page/html/${props.title}`);
+              const data = await response.text(); 
+              setContent(data);
+        };
+        fetchArticle();
+    }, [props.title]);
+
+    return (
+        <div className="article-container">
+            <p className="article_title">{props.title}</p>
+                <div 
+                    className="article-content"
+                    dangerouslySetInnerHTML={{ __html: content }}
+                />
+        </div>
+    );
+};
