b3e4395f936c039aae75c58ddd48f6c51412921c
Kamusubi <khephdjelidi@gmail.com>
2025-02-28 19:58:33 +0100

    monstre

--- src/assets/monster/images.ts
+++ src/assets/monster/images.ts
@@ -1,11 +1,25 @@
 import green from './green.svg';
 import damien from './damien.svg';
 import bibabo from './bibabo.svg';
+import benjamin from './benjamin.png';
+import cornu from './cornu.png';
+import daniel from './daniel.png';
+import degrado from './degrado.png';
+import neuille from './neuille.png';
+import pommier from './pommier.png';
+import titouan from './titouan.png';
 
 const images: Record<string, string> = {
-  green,
-  damien,
-  bibabo,
+green,
+damien,
+bibabo,
+benjamin,
+cornu,
+daniel,
+degrado,
+neuille,
+pommier,
+titouan,
 };
 
 export default images;
