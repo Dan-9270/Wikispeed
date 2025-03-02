95b0d69b9852297bb0fe1d73c40385f1e719866e
dnkee <147556640+dnkee@users.noreply.github.com>
2025-03-02 05:08:48 +0100

    responsive dylan

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,12 @@ import MultiGame from './MultiGame'
 import SoloCreation from './SoloCreation'
 import EndGame from './EndGame'
 import EndGameSolo from './EndGameSolo'
-
  
 import './style/App.css'
 import './style/wikispeed.css'
 import SoloGame from "./SoloGame";
+import MultiSharePhone from "./MultiSharePhone";
+import SoloCreationPhone from "./SoloCreationPhone";
 
 
 function App() {
@@ -24,11 +25,15 @@ function App() {
         <Route path="/homephone" element={<HomePhone />} />
         <Route path="/multicreation" element={<MultiCreation />} />
         <Route path="/multishare" element={<MultiShare />} />
+        <Route path="/multisharephone" element={<MultiSharePhone />} />
+
         <Route path="/multigame" element={<MultiGame />} />
         <Route path="/sologame" element={<SoloGame />} />
+        <Route path="/solocreationphone" element={<SoloCreationPhone />} />
         <Route path="/solocreation" element={<SoloCreation />} />
         <Route path="/endgame" element={<EndGame/>} />
         <Route path="/endgamesolo" element={<EndGameSolo/>} />
+
       </Routes>
     </Router>
   );
