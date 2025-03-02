e680678023d23dae2a00fc9d3bcf96a9208b0e09
dnkee <147556640+dnkee@users.noreply.github.com>
2025-03-02 20:19:04 +0100

    booss

--- src/component/RouteComponent.tsx
+++ src/component/RouteComponent.tsx
@@ -2,7 +2,7 @@ export const PlayButton = () => {
 
   return (
     <button className="PlayButton">
-      <a href="#selector">Play ! →</a>
+      <a href="#top">Play ! →</a>
     </button>
   );
 };
