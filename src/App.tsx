9084c40ac876fb29724d1ac408611ef4ea9073a7
dnkee <147556640+dnkee@users.noreply.github.com>
2025-03-05 19:01:49 +0100

    PLUS ULTRA

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from './Home'
 import HomePhone from './HomePhone'
 import MultiCreation from './MultiCreation'
@@ -7,20 +8,22 @@ import MultiGame from './MultiGame'
 import SoloCreation from './SoloCreation'
 import EndGame from './EndGame'
 import EndGameSolo from './EndGameSolo'
+import Pagea from './pagea'
+import Pageb from './pageb'
 import Test from './test'
  
 import './style/App.css'
 import './style/wikispeed.css'
 import SoloGame from "./SoloGame";
 import MultiSharePhone from "./MultiSharePhone";
-import SoloCreationPhone from "./SoloCreationPhone";
-import { AudioProvider } from './script/AudioContext'
 
+import { AudioProvider } from './script/AudioContext';
 
 function App() {
 
   return (
     <AudioProvider>
+    
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -28,16 +31,18 @@ function App() {
         <Route path="/multicreation" element={<MultiCreation />} />
         <Route path="/multishare" element={<MultiShare />} />
         <Route path="/multisharephone" element={<MultiSharePhone />} />
-
+        <Route path="/pagea" element={<Pagea />} />
+        <Route path="/pageb" element={<Pageb />} />
         <Route path="/multigame" element={<MultiGame />} />
         <Route path="/sologame" element={<SoloGame />} />
-        <Route path="/solocreationphone" element={<SoloCreationPhone />} />
+        
         <Route path="/solocreation" element={<SoloCreation />} />
         <Route path="/endgame" element={<EndGame/>} />
         <Route path="/endgamesolo" element={<EndGameSolo/>} />
         <Route path="/test" element={<Test/>}/>
         </Routes>
     </Router>
+   
     </AudioProvider>
     
 
